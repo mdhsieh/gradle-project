f93c5cbf42357109b425a276562019fe21cc1dd2
Michael Hsieh <mdhsieh8@gmail.com>
2020-06-05 20:49:28 -0700

    feat: Add Java library to final project.
    The Java library supplies the joke. Runs on API 28 emulator.

---
+++ FinalProject/jokesource/src/main/java/com/michaelhsieh/jokesource/JokeSource.java
@@ -0,0 +1,11 @@
+package com.michaelhsieh.jokesource;
+
+public class JokeSource {
+
+    private String joke = "What do you call an alligator detective? An investi-gator";
+
+    public String getJoke() {
+        return joke;
+    }
+
+}
