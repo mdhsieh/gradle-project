a5073f0b5d91b339e1d2c5aef0d906475745d98a
Michael Hsieh <mdhsieh8@gmail.com>
2020-06-06 18:02:11 -0700

    feat: Connect app to backend.
    Created the EndpointsAsyncTask given in sample code. Displays a
    toast saying hi. Runs on API 28 emulator.

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,19 +1,33 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.michaelhsieh.jokedisplay.JokeActivity;
 import com.michaelhsieh.jokesource.JokeSource;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
 
 import static com.michaelhsieh.jokedisplay.JokeActivity.KEY_JOKE;
-//import android.widget.Toast;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +62,53 @@ public class MainActivity extends AppCompatActivity {
 //        JokeSource jokeSource = new JokeSource();
 //        Toast.makeText(this, jokeSource.getJoke(), Toast.LENGTH_SHORT).show();
 
-        Intent jokeIntent = new Intent(this, JokeActivity.class);
+        /*Intent jokeIntent = new Intent(this, JokeActivity.class);
         JokeSource jokeSource = new JokeSource();
         jokeIntent.putExtra(KEY_JOKE, jokeSource.getJoke());
-        startActivity(jokeIntent);
+        startActivity(jokeIntent);*/
+
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+        Log.d(TAG, "started EndpointsAsyncTask");
     }
 
+}
 
+class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+    }
 }
