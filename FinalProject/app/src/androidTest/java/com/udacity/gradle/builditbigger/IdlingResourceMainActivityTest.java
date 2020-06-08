87dc7997d54bbf0b80aad1d7defc414fb774df71
Michael Hsieh <mdhsieh8@gmail.com>
2020-06-08 14:39:29 -0700

    test: Create idling resource test to check that joke is retrieved.

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/IdlingResourceMainActivityTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/IdlingResourceMainActivityTest.java
@@ -1,4 +1,11 @@
 package com.udacity.gradle.builditbigger;
 
-public class IdlingResourceMainActivityScreenTest {
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests that the Async task which starts when the button is clicked in MainActivity
+ *  retrieves a non-empty String.
+ */
+@RunWith(JUnit4.class)
+public class IdlingResourceMainActivityTest {
 }
